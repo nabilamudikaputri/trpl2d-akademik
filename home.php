20d2f840d945f72382fd5fb9595f08195727b83e
Nabila Mudika Putri <nabilamudikaputri@gmail.com>
2026-01-15 09:04:26 +0700

    UAS_edit_profile

--- home.php
+++ home.php
@@ -1,11 +1,38 @@
 <h1>Selamat Datang</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore commodi tempora voluptates 
-    voluptatibus expedita enim recusandae sit aliquid provident optio voluptatem, dignissimos 
-    blanditiis nostrum officiis similique id doloribus iure pariatur eius quisquam deleniti aperiam! 
-    Dolore aspernatur magnam neque odio, harum adipisci recusandae molestias, pariatur distinctio 
-    non ex dolores animi nisi dolorem. Reiciendis temporibus assumenda mollitia dolorum qui consequatur 
-    reprehenderit deleniti provident doloribus corporis. Molestias nihil odit impedit, consectetur officiis
-    veniam ad quia quod saepe aspernatur. Delectus vero deserunt esse, dolor cum repudiandae similique in 
-    fuga perspiciatis placeat nam adipisci aperiam, quas et consequuntur est unde illo quod necessitatibus. 
-    Placeat, nostrum?
+
+<p>
+    Gagasan pendirian Jurusan Teknologi Informasi sudah ada sejak awal tahun 2000, 
+    namun terkendala oleh beberapa pertimbangan, antara lain ketersediaan Sumber Daya Manusia 
+    dan infrastruktur.
+</p>
+
+<p>
+    Pada awal bulan Februari 2005, beberapa orang dosen yang antara lain terdiri dari 
+    Erwadi Bakar, Surfayondri, Andrizal, H. A. Mooduto, Yulindon, Ahmad Dahlan, 
+    Ronal Hadi, dan Rahmat Hidayat, atas dukungan yang kuat dari pimpinan 
+    Politeknik Suhendrik Hanwar serta pimpinan lainnya, menyusun proposal 
+    pendirian program studi yang berkaitan dengan Teknologi Informasi.
+</p>
+
+<p>
+    Dasar utama pendirian jurusan ini adalah keinginan Politeknik Unand Padang 
+    untuk ikut berkontribusi secara maksimal dalam pengembangan dan penerapan 
+    teknologi informasi melalui dunia pendidikan, serta sebagai langkah antisipasi 
+    terhadap perkembangan teknologi informasi dan komunikasi yang sangat pesat 
+    dan membutuhkan sumber daya manusia yang kompeten.
+</p>
+
+<p>
+    Program studi yang diusulkan pada waktu itu adalah Teknik Komputer, 
+    Teknik Informatika, dan Manajemen Informatika. Alhamdulillah, dalam waktu 
+    yang tidak terlalu lama, izin pendirian program studi ini disetujui oleh DIKTI 
+    untuk dua program studi, yaitu Teknik Komputer dan Manajemen Informatika. 
+    Hal ini merupakan bukti kepercayaan DIKTI kepada Politeknik Unand Padang.
+</p>
+
+<p>
+    Penerimaan mahasiswa baru angkatan pertama dilaksanakan pada bulan 
+    September 2005, meskipun terlambat dua minggu dibandingkan dengan jurusan lainnya. 
+    Atas kesepakatan tim, pada tahap awal pengelolaan jurusan ini dipercayakan kepada 
+    Erwadi Bakar sebagai Ketua Jurusan dan Ronal Hadi sebagai Sekretaris Jurusan.
 </p>
--- index.php
+++ index.php
@@ -1,86 +1,101 @@
 <?php
-//session  | cookies
-
 session_start();
-//cek login sudah atau atau belum
-if(!isset($_SESSION['login'])){
-    header("Location: login.php");
-}
 
+// cek login
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit; // agar tidak lanjut ke bawah
+}
 ?>
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Mahasiswa</title>
+    <title>List Data Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-info navbar-light">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">Akademik</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<nav class="navbar navbar-expand-lg bg-info">
+    <div class="container">
+        <a class="navbar-brand" href="#"><h3>Akademik</h3></a>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=datamhs">Data Mahasiswa</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php?page=prodi">Data Prodi</a>
-                    </li>
-                </ul>
-                <!-- LOGOUT BUTTON -->
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="btn btn-danger btn-sm"
-                        href="logout.php"
-                        onclick="return confirm('Yakin ingin logout?')"> Logout </a>
-                    </li>
-                </ul>
-            </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=mahasiswa">Data Mahasiswa</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?page=prodi">Program Studi</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto align-items-center">
+                <!-- EDIT PROFIL -->
+                <li class="nav-item">
+                    <a class="btn btn-dark btn-sm me-2" href="index.php?page=profil"><i class="bi bi-person-circle me-1"></i>Edit Profil</a>
+                </li>
+                
+
+
+                <li class="nav-item ms-3">
+                    <a class="btn btn-danger btn-sm" href="logout.php" onclick="return confirm('Yakin ingin keluar?')"> Logout </a>
+                </li>
+            </ul>
         </div>
-    </nav>
-
-    <div class="container my-4">
-        <?php
-            $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-            switch ($page) {
-                case 'home':
-                    include 'home.php';
-                    break;
-                case 'datamhs':
-                    include 'mahasiswa/list.php';
-                    break;
-                case 'mahasiswa_create':
-                    include 'mahasiswa/create.php';
-                    break;
-                case 'mahasiswa_update':
-                    include 'mahasiswa/edit.php';
-                    break;
-                case 'prodi':
-                    include 'prodi/list.php';
-                    break;
-                case 'prodi_create':
-                    include 'prodi/create.php';
-                    break;
-                case 'prodi_update':
-                    include 'prodi/edit.php';
-                    break;
-                default:
-                    include 'home.php';
-            }
-        ?>
     </div>
+</nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<div class="container my-4">
+<?php
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+    switch ($page) {
+        case 'home':
+            include 'home.php';
+            break;
+        case 'mahasiswa':
+            include 'mahasiswa/list.php';
+            break;
+        case 'mahasiswa_create':
+            include 'mahasiswa/create.php';
+            break;
+        case 'mahasiswa_update':
+            include 'mahasiswa/edit.php';
+            break;
+        case 'prodi':
+            include 'prodi/list.php';
+            break;
+        case 'prodi_create':
+            include 'prodi/create.php';
+            break;
+        case 'prodi_update':
+            include 'prodi/edit.php';
+            break;
+        case 'profil':
+            include 'edit_profil.php';
+            break;
+        default:
+            include 'home.php';
+    }
+?>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
